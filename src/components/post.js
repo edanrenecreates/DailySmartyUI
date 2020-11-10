36900c26ef399361d727f3ab31b12e9c81412ed6
edanrenecreates <rene@edanramos.com>
2020-11-09 20:11:17 -0700

    finished styling the result post component

--- src/components/post.js
+++ src/components/post.js
@@ -14,7 +14,7 @@ class Post extends Component {
   renderTopics() {
     let topics = this.props.associated_topics.map((topic, index) => {
       return <span className="post-topic" key={index}>{topic}</span>
-    }) 
+    })
     return topics;
   }
 
@@ -24,16 +24,16 @@ class Post extends Component {
         <div className='post-link' key={index}>
           <div className="post-link__box"></div>
           <div className="post-link__link">
-      <a href={post_link.link_url}>Useful Link #{index + 1}</a>
+            <a href={post_link.link_url}>Useful Link #{index + 1}</a>
           </div>
-        </div> 
+        </div>
       )
     })
     return links;
   }
 
   render() {
-    if(this.props.type == 'recent') {
+    if (this.props.type == 'recent') {
       return (
         <li className="recent-post">
           <div className="recent-post__title">
@@ -43,32 +43,33 @@ class Post extends Component {
             {this.renderTopics()}
           </div>
         </li>
-        )
-      } else if(this.props.type == 'result') {
-          return (
-            <li className='result-post'>
-              <div className='result-post__topics'>
-                {this.renderTopics()}
-              </div>
-              <div className='result-post__title'>
-                <a href={this.props.url_for_post}
-                  onMouseEnter={() => this.setState({ height: 70 })}
-                  onMouseLeave={() => this.setState({ height: 0 })}
-                >
-                {this.props.title}</a>
-              </div>
-              <AnimateHeight
-                duration={500}
-                height={this.state.height}
-              >
-                <div className='result-post__links'>
-                  {this.renderLinks()}
-                </div>
-              </AnimateHeight>
-            </li> 
-          )
-        }
+      )
+    } else if (this.props.type == 'result') {
+      return (
+        <li className='result-post'
+          onMouseEnter={() => this.setState({ height: 70 })}
+          onMouseLeave={() => this.setState({ height: 0 })}        
+        >
+
+          <div className='result-post__topics'>
+            {this.renderTopics()}
+          </div>
+          <div className='result-post__title'>
+            <a href={this.props.url_for_post}>
+              {this.props.title}</a>
+          </div>
+          <AnimateHeight
+            duration={500}
+            height={this.state.height}
+          >
+            <div className='result-post__links'>
+              {this.renderLinks()}
+            </div>
+          </AnimateHeight>
+        </li>
+      )
     }
+  }
 }
 
 export default Post;
